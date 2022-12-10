6ed11f27347ba5f5db817f0561883614dfe09cc2
pwnd4 <33100474+StickyKeep@users.noreply.github.com>
2022-12-10 22:59:51 +0100

    initial commit

---
+++ Day 7/day7.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+path = []
+size = 0
+SIZE = defaultdict(int)
+
+with open("Day 7/input.txt", "r") as f:
+    lineList = [line.strip().split(" ") for line in f]
+
+for element in lineList:
+    if element[0] == "$":
+        if element [1] == "cd":
+            if element[2] != "..":
+                path.append(element[2])
+                size = 0
+            else:
+                path.pop() 
+                size = 0
+    elif element[0] == "dir":
+        continue
+    else:
+        size = int(element[0])
+        for i in range(0,len(path)):
+                SIZE['/'.join(path[:i+1])] += size
+addorz = 0
+for value in SIZE.values(): 
+    if value <= 100000:
+        addorz += value
+
+print(f"part 1: {addorz}")
+
+# gives correct answer: 1334506
